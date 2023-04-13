46b2e08cc0617157134af667c7c0d2d33d83c611
Daria1508 <130033981+Daria1508@users.noreply.github.com>
2023-04-13 20:52:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import scipy.stats as st
 chat_id = 1307537098 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    p_value=st.ks_2samp(x, y, alternative='two-sided')[1]
+    p_value = st.cramervonmises_2samp(x, y)[1]
     if (p_value<0.09):
         return True
     else:
