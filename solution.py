7b6eb2cbf87ad269eb3aba153bc0ea00898bae7b
Daria1508 <130033981+Daria1508@users.noreply.github.com>
2023-04-13 20:01:28 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,8 +5,7 @@ import scipy.stats as st
 chat_id = 1307537098 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    p_v=st.ks_2samp(x, y, alternative='two-sided')[1]
-    p_value=2*np.min([norm.cdf(p_v)-
+    p_value=st.ks_2samp(x, y, alternative='two-sided')[1]
     if (p_value<0.09):
         return True
     else:
